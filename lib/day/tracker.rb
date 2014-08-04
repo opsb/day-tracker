ca7483d263040babc638203f0065df1604a01d94
opsb <oliver@opsb.co.uk>
2014-08-04 09:53:16 -0500

    Add fractions

--- lib/day/tracker.rb
+++ lib/day/tracker.rb
@@ -3,7 +3,7 @@ require "day/tracker/version"
 module Day
   module Tracker
   	class Cli < Thor
-  		FILE_PATH = File.expand_path("~/day-tracker")
+  		FILE_PATH = File.expand_path("~/.day-tracker")
 
   		def initialize(*args)
 		  super
@@ -15,12 +15,35 @@ module Day
   			puts File.read(FILE_PATH)
   		end
 
-  		desc "add PROJECT_NAME", "add a project for today"
-  		def add(project_name)
+  		desc "add PROJECT_NAME [FRACTION]", "add a project for today"
+  		def add(project, fraction=1)
+  			entry = format_entry(project, fraction)
+  			return if entry_exists?(entry)
+
   			open(FILE_PATH, 'a') do |file|
-  				file.puts Time.now.strftime("%B %d - #{project_name}")
+  				file.puts entry
   			end
+
+  			list
   		end
+
+  		no_commands do
+	  		def entry_exists?(entry)
+	  			File.readlines(FILE_PATH).grep(entry).any?
+	  		end
+
+	  		def format_entry(project, fraction)
+	  			Time.now.strftime("%B %d - #{project} - #{formatted_fraction(fraction)}")
+	  		end
+
+	  		def formatted_fraction(fraction)
+	  			{
+					"0.5": "half",
+					".5": "half",
+					"1", "full"	  				
+	  			}[fraction]	  			
+	  		end
+	  	end
 
   		default_task :list
   	end
