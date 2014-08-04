8bf26901a57d7881d1cf59b91490c788d9393344
opsb <oliver@opsb.co.uk>
2014-08-04 09:03:26 -0500

    Initial commit

---
+++ lib/day/tracker.rb
@@ -0,0 +1,7 @@
+require "day/tracker/version"
+
+module Day
+  module Tracker
+    # Your code goes here...
+  end
+end
---
+++ lib/day/tracker/version.rb
@@ -0,0 +1,5 @@
+module Day
+  module Tracker
+    VERSION = "0.0.1"
+  end
+end
