7ce3de632b87a2dc889a7ab6bfbf8fa02e7eb1dc
opsb <oliver@opsb.co.uk>
2014-08-04 09:29:52 -0500

    Add 'list' and 'add' commands

--- lib/day/tracker.rb
+++ lib/day/tracker.rb
@@ -3,9 +3,26 @@ require "day/tracker/version"
 module Day
   module Tracker
   	class Cli < Thor
-  		def hello
-  			puts "hello"
+  		FILE_PATH = File.expand_path("~/day-tracker")
+
+  		def initialize(*args)
+		  super
+		  FileUtils.touch FILE_PATH
+		end
+
+  		desc "list", "list recorded days"
+  		def list
+  			puts File.read(FILE_PATH)
   		end
+
+  		desc "add PROJECT_NAME", "add a project for today"
+  		def add(project_name)
+  			open(FILE_PATH, 'a') do |file|
+  				file.puts Time.now.strftime("%B %d - #{project_name}")
+  			end
+  		end
+
+  		default_task :list
   	end
   end
 end
